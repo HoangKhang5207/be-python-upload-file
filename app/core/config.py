ce8f15a8053301b056cd9aa9e6f8c58c823790bc
namsaber <namdotatks48@gmail.com>
2025-10-29 19:14:05 +0700

    (Feat)Add services pgadmin in docker, edit config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pydantic import BaseSettings
+from typing import Optional
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
@@ -15,7 +16,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 7  # Token expired after 7 days
     SECURITY_ALGORITHM = 'HS256'
     LOGGING_CONFIG_FILE = os.path.join(BASE_DIR, 'logging.ini')
-    ACCESS_TOKEN_PUBLIC_KEY = os.getenv('ACCESS_TOKEN_PUBLIC_KEY')
+    ACCESS_TOKEN_PUBLIC_KEY: Optional[str] = os.getenv('ACCESS_TOKEN_PUBLIC_KEY')
     MODEL = os.getenv('MODEL')
     ELASTICSEARCH_ENPOINT = os.getenv('ELASTICSEARCH_ENPOINT')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
